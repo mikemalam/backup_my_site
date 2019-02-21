4ef5acab8d190f196081ae025a6e6b9b49d72bd5
mikemalam <mikemalam@tutanota.com>
2019-02-21 15:37:20 -0500

    added logging code

--- update.py
+++ update.py
@@ -1,5 +1,21 @@
 #! /usr/bin/env python
 # -*- utf-8 -*-
-# update.py - See README.md for description.
+# update.py - See README.md for description
+
+import os
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+
+# TODO: Create a zip file of the directory and the database
+
+logging.debug('Start of program')
+
+    # TODO: Backup the Database
 
 
+    # TODO: Create the ZIP file
+
+# TODO: Upload the file on different locations
+
+# TODO: Inform me il the job is done or not
